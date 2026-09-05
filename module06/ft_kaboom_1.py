9f4c690e17d55f1341f317435566b7419b138476
bdubovik <bdubovik@student.42warsaw.pl>
2026-09-05 22:53:47 +0200

    changed finished module 6

--- module06/ft_kaboom_1.py
+++ module06/ft_kaboom_1.py
@@ -1,14 +1,16 @@
-from grimoire import dark_spellbook
-
-
 def main() -> None:
     print("=== Kaboom 1 ===")
     print("Access to alchemy/grimoire/dark_spellbook.py directly")
     print("Test import now - THIS WILL RAISE AN UNCAUGHT EXCEPTION")
     ing = 'Earth wind fire'
-    print(
-        f"Testing record light spell: "
-        f"{dark_spellbook('Fantasy', ing)}")
+
+    try:
+        from grimoire import dark_spellbook
+        print(
+            f"Testing record light spell: "
+            f"{dark_spellbook.dark_spell_record('Fantasy', ing)}")
+    except ImportError as e:
+        print(e)
 
 
 main()
