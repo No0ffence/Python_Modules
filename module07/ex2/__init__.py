7a1fff8bd7942d1220f8ff7fee429ddefe667ebe
bdubovik <bdubovik@student.42warsaw.pl>
2026-09-06 02:48:17 +0200

    mypy m7

--- module07/ex2/__init__.py
+++ module07/ex2/__init__.py
@@ -1,3 +1,11 @@
-from .strategies import (AggressiveStrategy, NormalStrategy,
-                         DefensiveStrategy, BattleStrategy)
-from .error_strategy_exception import InvalidStrategyError
+from .strategies import (AggressiveStrategy, NormalStrategy,  # noqa: F401
+                         DefensiveStrategy, BattleStrategy)  # noqa: F401
+from .error_strategy_exception import InvalidStrategyError  # noqa: F401
+
+__all__ = [
+    "NormalStrategy",
+    "DefensiveStrategy",
+    "AggressiveStrategy",
+    "BattleStrategy",
+    "InvalidStrategyError",
+]
