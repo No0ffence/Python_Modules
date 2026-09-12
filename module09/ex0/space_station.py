99b6bb4d72e4335ab2accaa12ac67f25a259d115
bdubovik <bdubovik@student.42warsaw.pl>
2026-09-12 09:32:34 +0200

    finished ex0

--- module09/ex0/space_station.py
+++ module09/ex0/space_station.py
@@ -12,6 +12,18 @@ class SpaceStation(BaseModel):
     is_operational: bool = Field(default=True)
     notes: str | None = Field(default=None, max_length=200)
 
+    def show(self):
+        print("Valid station created:")
+        print(f"ID: {self.station_id}")
+        print(f"Name: {self.name}")
+        print(f"Crew: {self.crew_size} people")
+        print(f"Power: {self.power_level} %")
+        print(f"Oxygen: {self.oxygen_level} %")
+        if self.is_operational:
+            print("Status: Operational")
+        else:
+            print("Status: Not operational")
+
 
 def main():
     station1 = SpaceStation(station_id="ISS001",
@@ -20,6 +32,24 @@ def main():
                             power_level=85.5,
                             oxygen_level=92.3,
                             last_maintenance=datetime.now())
+
+    print("""
+Space Station Data Validation
+========================================""")
+    station1.show()
+
+    print("""
+========================================
+Expected validation error:""")
+    try:
+        station2 = SpaceStation(station_id="ISS001455654546",
+                                name="International Space Station",
+                                crew_size=6,
+                                power_level=85.5,
+                                oxygen_level=92.3,
+                                last_maintenance=datetime.now())
+    except ValueError as e:
+        print(e)
 
 
 if __name__ == "__main__":
